47c4577ea31e5a2c202beeb6f83454e6fe289cdc
Hridoy Mnd <hridoymnd10@gmail.com>
2025-04-18 18:18:33 +0600

    jwt token create and use

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 import express from "express";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import cors from "cors";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4120;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser())
+
+
+
+// token veryfiy middleware
+const veryfyToken = (req, res, next) => {
+  const jotoken = req.cookies?.token;
+  if(!jotoken){
+   return res.status(401).send({message:"Unothorized token"})
+  }
+
+  jwt.verify(jotoken, process.env.JWT_SECRET, (err, decoded)=> {
+    if(err) {
+      return res.status(401).send({message: "Unothorized Acess"})
+    }
+    req.user =decoded;
+    next();
+  })
+
+  // console.log("now inside the middleware");
+  // console.log(coke);
+  // next();
+
+/**data access korar somoy 
+ * je api ee secure korte chai sekhane middle ware ta call korte hobe
+ * jode email diye data access korte chai tahole joken er email 
+ * and api te pathano email match korte hobe
+ */
+
+}
+
+
+
 
 // listner
 app.listen(port, () => {
@@ -55,25 +94,47 @@ async function run() {
       }
     });
 
+    // jwt token create
+    // app.post("/jwt_token", (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(
+    //     user,
+    //     process.env.JWT_SECRET,
+    //     { expiresIn: "1h" },
+    //   );
+    //   res.cookie("token", token, {
+    //     httpOnly: true,
+    //     secure:false,
+    //   })
+    //   .send({success:true})
+    // });
+
+    // jwt token create
+    app.post("/jot", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res.cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     // get all room
-    app.get("/all_room", async (req, res) => {
+    app.get("/all_room", veryfyToken, async (req, res) => {
       try {
+        // console.log("now inside the all_room api");
+        // const  coke = req.cookies;
+        // console.log(coke);
+
+        // if(req.user.email !==)
+
         const data = await RoomCollection.find().toArray();
         res.status(201).send(data);
+        // console.log("cuk cuk", req.cookies);
       } catch (err) {
         res.status(500).json({ message: "All room fetch failed", error: err });
       }
-    });
-
-    // jwt token create
-    app.post("/jwt_token", (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(
-        user,
-        "secret",
-        { expiresIn: "1h" },
-      );
-      res.send(token)
     });
 
     // add a new booked
